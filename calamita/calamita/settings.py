d34ffbd6f8e3957767a8c628f92ed07b126e49ad
rinmiura <121084976+rinmiura@users.noreply.github.com>
2023-04-19 16:56:40 +0300

    Update settings.py

--- calamita/calamita/settings.py
+++ calamita/calamita/settings.py
@@ -136,8 +136,8 @@ AUTH_USER_MODEL = 'account.User'
 # smtp
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'josh0hild@gmail.com'
-EMAIL_HOST_PASSWORD = 'lrnjhvzdcqhwmfbz'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = '587'
 EMAIL_USE_TLS = True
 
